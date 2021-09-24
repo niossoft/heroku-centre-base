87d34f7ad3fcc3f7c948f5736114e19409af6b9c
niossoft <niossoft@gmail.com>
2021-09-25 01:26:34 +0800

    init heroku-centre-base project!

---
+++ heroku-centre-base/src/main/java/com/laubang/HerokuCentreBaseApplication.java
@@ -0,0 +1,13 @@
+package com.laubang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuCentreBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuCentreBaseApplication.class, args);
+	}
+
+}
---
+++ heroku-centre-base/src/main/java/com/laubang/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.laubang;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("welcome")
+	public String welcome() {
+		return "Welcome to my heroku-centre-base for Spring Boot!!!";
+	}
+}
